82730861c2246e563f9c4dd1405dcc8e4254e905
Deekshay22 <92843554+Deekshay22@users.noreply.github.com>
2023-08-07 15:22:20 +0530

    Add files via upload

---
+++ Machine-Learning-Based-Insurance-Fraud-Detection-main/README.md
@@ -0,0 +1,147 @@
+# Insurance Fraud Detection with Machine Learning
+
+![GitHub last commit](https://img.shields.io/github/last-commit/Deepesh3104/Machine-Learning-Based-Insurance-Fraud-Detection)
+![GitHub issues](https://img.shields.io/github/issues/Deepesh3104/Machine-Learning-Based-Insurance-Fraud-Detection)
+![GitHub stars](https://img.shields.io/github/stars/Deepesh3104/Machine-Learning-Based-Insurance-Fraud-Detection)
+![GitHub forks](https://img.shields.io/github/forks/Deepesh3104/Machine-Learning-Based-Insurance-Fraud-Detection)
+![GitHub license](https://img.shields.io/github/license/Deepesh3104/Machine-Learning-Based-Insurance-Fraud-Detection)
+
+## Overview
+
+Welcome to the extraordinary realm of Insurance Fraud Detection with Machine Learning. Prepare to embark on a journey of unparalleled innovation and cutting-edge technology as you dive into this transformative repository. Created by the ingenious minds of **Deepesh** and **Deeksha Yadav** at the prestigious Indian Institute of Technology (IIT) Bhilai, this project stands as a beacon of hope in safeguarding the insurance industry from deceptive practices.
+
+## Table of Contents
+
+- [Introduction](#introduction)
+- [Ensemble Learning and the Voting Classifier](#ensemble-learning-and-the-voting-classifier)
+- [Requirements](#requirements)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+- [Contact](#contact)
+
+## Introduction
+
+Amidst the dynamic landscape of the insurance industry lies the daunting challenge of fraudulent activities. Our project is a testament to the fusion of machine learning brilliance and ethical vision, as we leverage advanced techniques to build a predictive model capable of unearthing potential fraudulent insurance claims with unprecedented accuracy.
+
+## Ensemble Learning and the Voting Classifier
+
+Elevating our project to new heights is the revolutionary Ensemble Learning technique. By assembling an array of diverse machine learning algorithms, we create a coalition of predictive power. The wisdom of each model harmonizes with the others under the expert guidance of the Voting Classifier, ensuring a collective decision that surpasses the capabilities of any single model.
+
+### Benefits of Ensemble Learning
+
+Embracing Ensemble Learning provides our system with an array of advantages:
+
+1. **Unprecedented Accuracy:** By harnessing the collective intelligence of multiple models, our predictions reach unparalleled levels of precision and trustworthiness.
+
+2. **Robustness in Complexity:** In the face of intricate and challenging scenarios, our system remains steadfast and reliable, fortified by the diverse strengths of its constituent models.
+
+3. **Resilience against Overfitting:** The Ensemble Learning approach diminishes the risk of overfitting, ensuring that our model generalizes efficiently to new and unseen data.
+
+## Requirements
+
+To embark on this transformative journey, equip yourself with the following dependencies:
+
+- Flask==2.3.2 
+- scikit-learn==1.0.2
+- pandas==1.5.1
+- Python
+- bcrypt
+- pymongo==4.4.1
+
+Please install these specific versions of the required packages to avoid compatibility issues.
+
+## Installation
+
+Prepare to immerse yourself in this awe-inspiring creation with the following steps:
+
+1. Install Python:
+   - Go to the official Python website (https://www.python.org/downloads/) and download the appropriate Python version for your operating system (Windows, macOS, or Linux).
+   - Run the installer and follow the on-screen instructions to complete the installation.
+
+2. Install Visual Studio (for Windows users):
+   - Visual Studio is required to build Python extensions on Windows. You can download the Visual Studio installer from the official website: https://visualstudio.microsoft.com/downloads/
+   - Launch the Visual Studio Installer and select "Desktop development with C++" workload.
+   - Click on "Install" to install the necessary components for Python development.
+
+3. Install Tailwind CSS (Optional):
+   - To use Tailwind CSS in your project, you need Node.js and npm (Node Package Manager) installed. If you don't have Node.js and npm, download and install them from the official website: https://nodejs.org/en/download/
+   - Once Node.js and npm are installed, open your terminal/command prompt and navigate to the root of your project directory.
+   - Run the following command to initialize a new `package.json` file:
+     ```bash
+     $ npm init -y
+     ```
+   - Now, install Tailwind CSS and its dependencies by running the following command:
+     ```bash
+     $ npm install tailwindcss postcss autoprefixer
+     ```
+   - Next, create a configuration file for Tailwind CSS by running:
+     ```bash
+     $ npx tailwindcss init
+     ```
+   - This will create a `tailwind.config.js` file in your project directory.
+
+   **Note: The installation of Tailwind CSS is optional and only required if you want to customize the website's design. In some cases, the default design may work perfectly fine without installing Tailwind CSS. If you encounter any design issues, consider installing Tailwind CSS manually.**
+
+4. Clone the repository:
+
+```bash
+$ git clone
+
+ https://github.com/Deepesh3104/Machine-Learning-Based-Insurance-Fraud-Detection.git
+```
+
+5. Navigate to the project directory:
+
+```bash
+$ cd Machine-Learning-Based-Insurance-Fraud-Detection
+```
+
+6. Install the required Python dependencies:
+
+```bash
+$ pip install -r requirements.txt
+```
+
+7. Launch the application:
+
+```bash
+$ python main.py
+```
+
+**Note: If you encounter any issues during the installation process, ensure that you have installed all the required dependencies listed in the [Requirements](#requirements) section.**
+
+## Usage
+
+Experience the marvels of this innovation by embarking on a user-friendly quest:
+
+1. Ensure that the dependencies outlined in the [Requirements](#requirements) section are installed in your environment.
+
+2. Launch the application:
+
+```bash
+$ python main.py
+```
+
+3. Interact with the dynamic interface, inputting vital data for insurance claims to let the Machine Learning model unveil the truth.
+
+## Contributing
+
+While we appreciate your passion for excellence and desire to contribute, this project stands as the proprietary masterpiece of IIT Bhilai. Though your insights and suggestions are treasured, the realm of pull requests shall remain untouched.
+
+## License
+
+This grand undertaking is the intellectual property of the Indian Institute of Technology (IIT) Bhilai and gracefully adorns the IIT Bhilai License. Consult the [LICENSE](LICENSE) file for the profound nuances of this alliance.
+
+## Contact
+
+For inquiries, licensing concerns, or profound matters related to this code, direct your missives to the esteemed custodians of knowledge:
+
+- Email: tih@iitbhilai.ac.in
+- Website: [IBITF Website](https://www.ibitf.co.in/)
+- Address: IBITF, IIT Bhilai, GEC Campus, Old Dhamatari Road, Sejbahar, Raipur, Chhattisgarh 492015, India
+
+---
+
+We extend our deepest gratitude to you, for venturing into the wondrous world of Insurance Fraud Detection with Machine Learning. With hearts full of gratitude, **Deepesh** and **Deeksha Yadav**, in unison with IIT Bhilai, aspire to usher in an era of fairness and efficiency in the insurance industry through the identification and prevention of fraudulent claims. Should you seek guidance or wish to share your reflections, hesitate not to connect. May this journey of discovery be forever etched in the annals of innovation. Happy detecting!
---
+++ Machine-Learning-Based-Insurance-Fraud-Detection-main/main.py
@@ -0,0 +1,411 @@
+from flask import (
+    Flask,
+    render_template,
+    request,
+    redirect,
+    flash,
+    session,
+    jsonify,
+    url_for,
+)
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+import pandas as pd
+import pickle
+from sklearn.metrics import accuracy_score
+from pymongo import MongoClient
+import bcrypt
+import pymongo
+import warnings
+import os
+
+
+app = Flask(__name__)
+
+# Load the individual models
+with open("gb_model.pkl", "rb") as file1:
+    gb_model = pickle.load(file1)
+
+with open("rf_model.pkl", "rb") as file:
+    rf_model = pickle.load(file)
+import secrets
+
+# Generate a random secret key of 32 bytes (256 bits)
+app.secret_key = os.urandom(24)
+
+# Mongo DB connnection Strings
+connection_string = "mongodb+srv://Deepesh2104:Deepesh2228@cluster0.7mgj9te.mongodb.net/deepesh?retryWrites=true&w=majority"
+# client = MongoClient(connection_string)
+# db = client.get_database('Deepesh2104')  # Database name
+# users_collection = db['users']
+
+try:
+    # Connect to MongoDB Atlas
+    client = pymongo.MongoClient(connection_string)
+
+    # Access your database (replace 'your-database' with the actual name)
+    db = client["deepesh"]
+
+    # Perform your database operations here...
+    # For example, you can retrieve a collection and fetch some data:
+    collection = db["user"]  # Replace 'your-collection' with the actual collection name
+
+    # Print the fetched data
+
+except pymongo.errors.ConnectionFailure as e:
+    print("Failed to connect to MongoDB Atlas. Error:", e)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/predict", methods=["GET", "POST"])
+def predict():
+
+    marital_status = request.form.get("MaritalStatus")
+    hours_week = request.form.get("Hours_Week")
+    gender = request.form.get("Gender")
+    occupation = request.form.get("Occupation")
+    customer_id = request.form.get("CustomerID")
+    age = request.form.get("Age")
+    months_as_customer = request.form.get("months_as_customer")
+    policy_deductable = request.form.get("policy_deductable")
+    policy_annual_premium = request.form.get("policy_annual_premium")
+    insured_relationship = request.form.get("insured_relationship")
+    capital_gains = request.form.get("capital-gains")
+    capital_loss = request.form.get("capital-loss")
+    incident_type = request.form.get("incident_type")
+    collision_type = request.form.get("collision_type")
+    incident_severity = request.form.get("incident_severity")
+    authorities_contacted = request.form.get("authorities_contacted")
+    number_of_vehicles_involved = request.form.get("number_of_vehicles_involved")
+    property_damage = request.form.get("property_damage")
+    bodily_injuries = request.form.get("bodily_injuries")
+    witnesses = request.form.get("witnesses")
+    police_report_available = request.form.get("police_report_available")
+    total_claim_amount = request.form.get("total_claim_amount")
+    injury_claim = request.form.get("injury_claim")
+    property_claim = request.form.get("property_claim")
+    vehicle_claim = request.form.get("vehicle_claim")
+    state = request.form.get("State")
+    vehicle_age = request.form.get("vehicle_age")
+    work = request.form.get("work")
+
+    # Check for missing form fields
+    if any(
+        value is None
+        for value in [
+            marital_status,
+            hours_week,
+            gender,
+            occupation,
+            customer_id,
+            age,
+            months_as_customer,
+            policy_deductable,
+            policy_annual_premium,
+            insured_relationship,
+            capital_gains,
+            capital_loss,
+            incident_type,
+            collision_type,
+            incident_severity,
+            authorities_contacted,
+            number_of_vehicles_involved,
+            property_damage,
+            bodily_injuries,
+            witnesses,
+            police_report_available,
+            total_claim_amount,
+            injury_claim,
+            property_claim,
+            vehicle_claim,
+            state,
+            vehicle_age,
+            work,
+        ]
+    ):
+        return render_template("prediction.html", prediction_text="Missing form fields")
+
+    # Convert the features to the appropriate format
+    marital_status = int(marital_status)
+    hours_week = float(hours_week)
+    gender = int(gender)
+    occupation = int(occupation)
+    customer_id = str(customer_id)
+    age = float(age)
+    months_as_customer = float(months_as_customer)
+    policy_deductable = float(policy_deductable)
+    policy_annual_premium = float(policy_annual_premium)
+    insured_relationship = int(insured_relationship)
+    capital_gains = float(capital_gains)
+    capital_loss = float(capital_loss)
+    incident_type = int(incident_type)
+    collision_type = int(collision_type)
+    incident_severity = int(incident_severity)
+    authorities_contacted = int(authorities_contacted)
+    number_of_vehicles_involved = float(number_of_vehicles_involved)
+    property_damage = float(property_damage)
+    bodily_injuries = float(bodily_injuries)
+    witnesses = float(witnesses)
+    police_report_available = int(police_report_available)
+    total_claim_amount = float(total_claim_amount)
+    injury_claim = float(injury_claim)
+    property_claim = float(property_claim)
+    vehicle_claim = float(vehicle_claim)
+    state = int(state)
+    vehicle_age = float(vehicle_age)
+    work = int(work)
+
+    # Perform predictions using individual models
+    gb_prediction = gb_model.predict(
+        [
+            [
+                marital_status,
+                hours_week,
+                gender,
+                occupation,
+                customer_id,
+                age,
+                months_as_customer,
+                policy_deductable,
+                policy_annual_premium,
+                insured_relationship,
+                capital_gains,
+                capital_loss,
+                incident_type,
+                collision_type,
+                incident_severity,
+                authorities_contacted,
+                number_of_vehicles_involved,
+                property_damage,
+                bodily_injuries,
+                witnesses,
+                police_report_available,
+                total_claim_amount,
+                injury_claim,
+                property_claim,
+                vehicle_claim,
+                state,
+                vehicle_age,
+                work,
+            ]
+        ]
+    )
+
+    rf_prediction = rf_model.predict(
+        [
+            [
+                marital_status,
+                hours_week,
+                gender,
+                occupation,
+                customer_id,
+                age,
+                months_as_customer,
+                policy_deductable,
+                policy_annual_premium,
+                insured_relationship,
+                capital_gains,
+                capital_loss,
+                incident_type,
+                collision_type,
+                incident_severity,
+                authorities_contacted,
+                number_of_vehicles_involved,
+                property_damage,
+                bodily_injuries,
+                witnesses,
+                police_report_available,
+                total_claim_amount,
+                injury_claim,
+                property_claim,
+                vehicle_claim,
+                state,
+                vehicle_age,
+                work,
+            ]
+        ]
+    )
+
+    # Perform majority voting ensemble
+    ensemble_prediction = []
+    for i in range(len(gb_prediction)):
+        if gb_prediction[i] + rf_prediction[i] >= 1:
+            ensemble_prediction.append(1)
+        else:
+            ensemble_prediction.append(0)
+
+    # Calculate ensemble accuracy using voting
+    ensemble_accuracy = accuracy_score(ensemble_prediction, gb_prediction)
+
+    # Process the ensemble prediction result and return it
+    result = "Fraud" if ensemble_prediction[0] == 1 else "Not fraud"
+    return render_template(
+        "prediction.html",
+        prediction_text="Prediction: {}".format(result),
+        ensemble_accuracy=ensemble_accuracy,
+    )
+
+
+@app.route("/import_data", methods=["GET", "POST"])
+def import_data():
+    # Check if a file is uploaded
+    if "file" not in request.files:
+        return render_template("Import.html", prediction_text="No file uploaded.")
+
+    file = request.files["file"]
+
+    # Check if the file is empty
+    if file.filename == "":
+        return render_template("Import.html", prediction_text="No file selected.")
+
+    # Read the CSV file
+    try:
+        data = pd.read_csv(file)
+    except Exception as e:
+        return render_template("Import.html", prediction_text="Error reading the file.")
+
+    # Perform predictions using individual models
+    gb_prediction = gb_model.predict(data)
+    rf_prediction = rf_model.predict(data)
+
+    # Perform majority voting ensemble
+    ensemble_prediction = []
+    for i in range(len(gb_prediction)):
+        if gb_prediction[i] + rf_prediction[i] >= 1:
+            ensemble_prediction.append(1)
+        else:
+            ensemble_prediction.append(0)
+
+    # Calculate ensemble accuracy using voting
+    ensemble_accuracy = accuracy_score(ensemble_prediction, gb_prediction)
+
+    # Process the ensemble prediction result and return it
+    result = ["Fraud" if pred == 1 else "Not fraud" for pred in ensemble_prediction]
+    return jsonify({"prediction_text": result, "ensemble_accuracy": ensemble_accuracy})
+
+
+@app.route("/login1", methods=["GET", "POST"])
+def login1():
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+        # Find the user with the provided email in the database
+        user = collection.find_one({"email": email})
+
+        if user and bcrypt.checkpw(password.encode("utf-8"), user["password"]):
+            # Store the user's email in the session to keep them logged in
+            session["user_id"] = str(user["_id"])
+            return redirect(url_for("dashboard"))
+        else:
+            return render_template("login1.html", error="Invalid credentials")
+
+    return render_template("login1.html")
+
+@app.route('/logout')
+def logout():
+    session.pop("user_id", None)
+    return render_template("login1.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        firstname = request.form["Firstname"]
+        lastname = request.form["Lastname"]
+        email = request.form["Email"]
+        password = request.form["Password"]
+        # Hash the password before storing it
+
+        existing_user = collection.find_one({"email": email})
+        if existing_user:
+            return render_template("login1.html", error="Email already exists")
+        hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+        user_data = {
+            "firstname": firstname,
+            "lastname": lastname,
+            "email": email,
+            "password": hashed_password,
+        }
+        collection.insert_one(user_data)
+
+        return redirect(url_for("login1"))
+
+    return render_template("login1.html")
+
+@app.route("/dashboard")
+def dashboard():
+    if "user_id" in session:
+        user_id = session["user_id"]
+        user = collection.find_one({"_id": ObjectId(user_id)})
+        return render_template("dashboard.html")
+    else:
+        flash("Please log in first", "warning")
+        return redirect(url_for("login1"))
+
+@app.route("/services", methods=["GET", "POST"])
+def services():
+    return render_template("services.html")
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    return render_template("contactus.html")
+
+
+@app.route("/about", methods=["GET", "POST"])
+def about():
+    return render_template("about.html")
+
+
+@app.route("/index", methods=["GET", "POST"])
+def index():
+    return render_template("index.html")
+
+
+@app.route("/blog1", methods=["GET", "POST"])
+def blog1():
+    return render_template("blog1.html")
+
+@app.route("/blog2", methods=["GET", "POST"])
+def blog2():
+    return render_template("blog2.html")
+
+@app.route("/blog3", methods=["GET", "POST"])
+def blog3():
+    return render_template("blog3.html")
+
+@app.route("/content1", methods=["GET", "POST"])
+def content1():
+    return render_template("content1.html")
+
+@app.route("/content2", methods=["GET", "POST"])
+def content2():
+    return render_template("content2.html")
+
+@app.route("/content3", methods=["GET", "POST"])
+def content3():
+    return render_template("content3.html")
+
+
+@app.route("/content4", methods=["GET", "POST"])
+def content4():
+    return render_template("content4.html")
+
+@app.route("/feature1", methods=["GET", "POST"])
+def feature1():
+    return render_template("feature1.html")
+
+@app.route("/feature2", methods=["GET", "POST"])
+def feature2():
+    return render_template("feature1.html")
+
+@app.route("/feature3", methods=["GET", "POST"])
+def feature3():
+    return render_template("feature3.html")
+
+
+if __name__ == "__main__":
+    app.run()
